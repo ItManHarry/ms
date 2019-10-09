d8370ddd60725c29895a540b4d589655e3fd6ed9
itmanharry <jack_chengqian@163.com>
2019-10-09 11:10:40 +0800

    monitor hystrix

--- ms_movie/src/main/java/com/doosan/ms/movie/MicroServiceMovieApplication.java
+++ ms_movie/src/main/java/com/doosan/ms/movie/MicroServiceMovieApplication.java
@@ -1,12 +1,15 @@
 package com.doosan.ms.movie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
@@ -25,5 +28,16 @@ public class MicroServiceMovieApplication {
 	@LoadBalanced
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet servlet = new HystrixMetricsStreamServlet();
+		@SuppressWarnings("unchecked")
+		ServletRegistrationBean registration = new ServletRegistrationBean(servlet);
+		registration.setLoadOnStartup(1);
+		registration.addUrlMappings("/hystrix.stream");
+		registration.setName("HystrixMetricsStreamServlet");
+		return registration;
 	}
 }

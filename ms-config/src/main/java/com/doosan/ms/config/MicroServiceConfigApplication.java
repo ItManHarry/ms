a3c2700b747380db7c75b4a43ec438a55c944b0c
itmanharry <jack_chengqian@163.com>
2019-10-16 15:49:10 +0800

    springcloud config

---
+++ ms-config/src/main/java/com/doosan/ms/config/MicroServiceConfigApplication.java
@@ -0,0 +1,12 @@
+package com.doosan.ms.config;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@SpringBootApplication
+@EnableConfigServer
+public class MicroServiceConfigApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceConfigApplication.class, args);
+	}
+}

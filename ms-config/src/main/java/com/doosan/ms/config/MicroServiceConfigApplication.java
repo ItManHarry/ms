f3194e3c9e36d3f3a8af4d6288ddb4d164445321
itmanharry <jack_chengqian@163.com>
2019-10-18 13:55:52 +0800

    config eureka

--- ms-config/src/main/java/com/doosan/ms/config/MicroServiceConfigApplication.java
+++ ms-config/src/main/java/com/doosan/ms/config/MicroServiceConfigApplication.java
@@ -2,8 +2,10 @@ package com.doosan.ms.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class MicroServiceConfigApplication {
 	
 	public static void main(String[] args) {
